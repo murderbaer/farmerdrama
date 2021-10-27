9ccb9effc74bc6198ca233b8f6631faaba9090d2
Jannik Münz <32801117+spontanurlaub@users.noreply.github.com>
2021-10-27 12:06:57 +0200

    Added Model View Controller Design Pattern

--- FarmGame/Program.cs
+++ FarmGame/Program.cs
@@ -2,61 +2,33 @@
 // Copyright (c) PlaceholderCompany. All rights reserved.
 // </copyright>
 
-using OpenTK.Graphics.OpenGL;
-using OpenTK.Mathematics;
 using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
 using OpenTK.Windowing.GraphicsLibraryFramework;
+using FarmGame;
 
 var window = new GameWindow(GameWindowSettings.Default, new NativeWindowSettings { Profile = ContextProfile.Compatability }); // window with immediate mode rendering enabled
+View view = new ();
+Model model = new ();
+Control control = new (model, view);
 
-float x = 0f;
 window.UpdateFrame += Update;
-window.Resize += Resize;
+window.Resize += view.Resize;
 window.KeyDown += args => { if (args.Key == Keys.Escape)
 {
     window.Close();
 }
 };
 
-window.RenderFrame += Draw; // called once each frame; callback should contain drawing code
+window.RenderFrame += _ => view.Draw(model); // called once each frame; callback should contain drawing code
 window.RenderFrame += _ => window.SwapBuffers(); // buffer swap needed for double buffering
 
-// setup code executed once
-GL.ClearColor(Color4.LightGray);
 
 window.Run();
-
-void Draw(FrameEventArgs args)
-{
-    // clear screen
-    GL.Clear(ClearBufferMask.ColorBufferBit);
-    GL.Color4(Color4.IndianRed);
-
-    // draw a quad
-    GL.Begin(PrimitiveType.Quads);
-    GL.Vertex2(0.0f, 0.0f); // draw first quad corner
-    GL.Vertex2(0.5f, 0.0f);
-    GL.Vertex2(0.5f, 0.5f);
-    GL.Vertex2(0.0f, 0.5f);
-    GL.End();
-}
-
-void Resize(ResizeEventArgs args)
-{
-    GL.Viewport(0, 0, args.Width, args.Height);
-}
 
 void Update(FrameEventArgs args)
 {
     var elapsedTime = (float)args.Time;
-    if (window.IsKeyDown(Keys.Left))
-    {
-        x -= elapsedTime;
-    }
-
-    if (window.IsKeyDown(Keys.Right))
-    {
-        x += elapsedTime;
-    }
+    control.Update(elapsedTime, window.KeyboardState);
+    model.Update(elapsedTime);
 }
